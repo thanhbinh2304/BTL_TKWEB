aae0801e9dc9fffe148f1c7de38500d69b8fb90d
thanhbinh2304 <binh2304tto@gmail.com>
2025-05-13 13:20:09 +0700

    them file loginjs + sua index

---
+++ docs/js/login.js
@@ -0,0 +1,58 @@
+function getUsers(){
+    return JSON.parse(localStorage.getItem("users")) || [];
+}
+function saveUsers(users) {
+    localStorage.setItem("users", JSON.stringify(users));
+}
+function register() {
+    const username = document.getElementById("newUser").value;
+    const password = document.getElementById("newPass").value;
+    const email = document.getElementById("newMail").value;
+    const passAgain = document.getElementById("PassAgain").value;
+    const users = getUsers();
+    const errors = [
+        { condition: username === "", message: "Vui lòng nhập Họ và Tên." },
+        { condition: email === "", message: "Vui lòng nhập email." },
+        { condition: password === "", message: "Vui lòng nhập mật khẩu." },
+        { condition: passAgain === "", message: "Vui lòng nhập lại mật khẩu." },
+        { condition: users.find(user => user.email === email), message: "Email đã được sử dụng." },
+        { condition: password !== passAgain, message: "Mật khẩu không khớp." }
+    ];
+    for(const error of errors) {
+        if (error.condition) {
+            alert(error.message);
+            return false;
+        }
+    }
+
+    const newUser = {email, password, username};
+    users.push(newUser);
+    saveUsers(users);
+    // Save the current user to localStorage
+    localStorage.setItem("user", JSON.stringify(newUser));
+    
+    alert("Đăng ký thành công!");
+    window.location.href = "login.html";    
+    return false;
+}
+function login() {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("pass").value;
+    
+    const users = getUsers();
+    const user = users.find(user => user.email === email);
+    if (!user) {
+        alert("Tài khoản không tồn tại.");
+        return false;
+    }
+    if (user.password !== password) {
+        alert("Mật khẩu không đúng.");
+        return false;
+    }
+    
+    // Save the logged-in user to localStorage
+    localStorage.setItem("user", JSON.stringify(user));
+    alert("Đăng nhập thành công!");
+    window.location.href = "index2.html";
+    return false;
+}
